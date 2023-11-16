06f0aa5b46bdffc7a74436c22cf4271dc1c197c4
M S I <M S I@MSI>
2023-11-16 12:07:55 +0700

    AddVariable

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -1,2 +1,25 @@
 package com.example.esjumbo
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+@Composable
+fun HalamanForm(
+    onSubmitButtonClick: (MutableList<String>) -> Unit
+){
+    var namaTxt by rememberSaveable {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var teleponTxt by remember {
+        mutableStateOf("")
+    }
+
+    var ListDataTxt: MutableList<String> = mutableListOf(namaTxt,alamatTxt,teleponTxt)
+}
